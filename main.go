7a723c04ebd1399f1401ff0e59bb75d07a944a8a
ak1103dev <ak.apichan1103@gmail.com>
2022-02-16 14:17:30 +0700

    :construction: add logic

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	var conferenceName = "Go Conference"
 	const conferenceTickets = 50
-	var remainingTickets = 50
+	var remainingTickets uint = 50
 
 	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
@@ -14,7 +14,7 @@ func main() {
 	fmt.Println("Get your tickets here to attend")
 
 	var userName string
-	var userTickets int
+	var userTickets uint
 
 	fmt.Print("Enter your name: ")
 	fmt.Scan(&userName)
@@ -22,5 +22,8 @@ func main() {
 	fmt.Print("Enter number of tickets: ")
 	fmt.Scan(&userTickets)
 
+	remainingTickets = remainingTickets - userTickets
+
 	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 }
