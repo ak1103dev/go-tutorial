18df9ee83019b38c63191ff92a07bac7b0079a62
ak1103dev <ak.apichan1103@gmail.com>
2022-02-16 14:54:23 +0700

    :construction: for

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	var conferenceName = "Go Conference"
@@ -14,29 +17,32 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still avaliable.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var firstName string
-	var lastName string
-	var userTickets uint
+	for {
+		var firstName string
+		var lastName string
+		var userTickets uint
 
-	fmt.Print("Enter your first name: ")
-	fmt.Scan(&firstName)
+		fmt.Print("Enter your first name: ")
+		fmt.Scan(&firstName)
 
-	fmt.Print("Enter your last name: ")
-	fmt.Scan(&lastName)
+		fmt.Print("Enter your last name: ")
+		fmt.Scan(&lastName)
 
-	fmt.Print("Enter number of tickets: ")
-	fmt.Scan(&userTickets)
+		fmt.Print("Enter number of tickets: ")
+		fmt.Scan(&userTickets)
 
-	remainingTickets = remainingTickets - userTickets
-	bookings = append(bookings, firstName + " " + lastName)
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, firstName + " " + lastName)
 
-	fmt.Printf("The whole slice: %v\n", bookings)
-	fmt.Printf("The first value: %v\n", bookings[0])
-	fmt.Printf("Slice Type: %T\n", bookings)
-	fmt.Printf("Slice length: %v\n", len(bookings))
+		fmt.Printf("User %v %v booked %v tickets.\n", firstName, lastName, userTickets)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
-	fmt.Printf("User %v %v booked %v tickets.\n", firstName, lastName, userTickets)
-	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+		firstNames := []string{}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
 
-	fmt.Printf("These are all our bookins: %v\n", bookings)
+		fmt.Printf("The first names of bookings are: %v\n", firstNames)
+	}
 }
